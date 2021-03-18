c761987851073cfea25cc79018ea29efca005c84
7147 <7147@C06-iMAC01.local>
2021-03-18 11:53:26 +1100

    CREATE footer.scss, flex-container.scss

--- inc/footer.php
+++ inc/footer.php
@@ -6,49 +6,46 @@
 
     <section class="footer-content">
 
-        <!--Left side of the footer-->
-        <div class="footer-content__left">
-            <h3>FIND US ON</h3>
-            <div class="footer_social">
-                <div class="social">
-                    <a href="#" class="fa fa-twitter" style="color:black"></a>
-                    <h5>Twitter</h5>
-                </div>
-                <div class="social">
-                    <a href="#" class="fa fa-linkedin" style="color:black"></a>
-                    <h5>Linkedin</h5>
-                </div>
-                <div class="social">
-                    <a href="#" class="fa fa-facebook" style="color:black"></a>
-                    <h5>Facebook</h5>
-                </div>
-            </div>
-        </div>
-
         <!--Right side of the footer-->
         <div class="footer-content__right">
-            <h3>CONTACT US</h3>
-
-            <ul class="">
+            <h3 class="footer-content__title">find us on</h3>
+            <ul class="icon-wrapper flex-container">
                 <li>
                     <a href="#"><i class="fab fa-twitter"></i></a>
                     <p>Twitter</p>
                 </li>
                 <li>
                     <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <p>Facebook</p>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
                     <p>Linkedin</p>
+                </li>
+            </ul>
+        </div>
+
+
+        <!--Left side of the footer-->
+        <div class="footer-content__left">
+            <h3 class="footer-content__title">contact us on </h3>
+            <ul class="icon-wrapper flex-container">
+                <li>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <p>Twitter</p>
                 </li>
                 <li>
                     <a href="#"><i class="fab fa-facebook-f"></i></a>
                     <p>Facebook</p>
                 </li>
+                <li>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    <p>Linkedin</p>
+                </li>
             </ul>
-
-
-            <h5>{phone number}<a href="#"><span class="glyphicon glyphicon-headphones" style="color:black"></span></a></h5>
-            <h5>gearup@support.nsw.au<a href="#"><span class="glyphicon glyphicon-envelope" style="color:black"></span></a></h5>
-            <h5>7 kelly st, Ultimo NSW 2007<a href="#"><span class="glyphicon glyphicon-map-marker" style="color:black"></span></a></h5>
         </div>
+
+        
 
     </section>
 </footer>
