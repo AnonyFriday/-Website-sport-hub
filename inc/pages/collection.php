e0da18d162961ff9ed749b325201c39e5a28140f
Vu Kim Duy <duyvukim752000@gmail.com>
2021-03-24 01:14:17 +1100

    [CREATE] summary and content, collection.php file successfully

--- inc/pages/collection.php
+++ inc/pages/collection.php
@@ -9,27 +9,55 @@
                     <span>Gender</span>
                 </summary>
                 <form class="details__content">
-                    <input type="checkbox" name="gender" value="male" checked></input>
+                    <div class="checkbox-wrapper">
+                        <input type="checkbox" name="gender" value="man">
+                        <span>Man</span>
+                    </div>
+
+                    <div class="checkbox-wrapper">
+                        <input type="checkbox" name="gender" value="woman">
+                        <span>Woman</span>
+                    </div>
+                </form>
+            </details>
+            
+            <details class="details__item">
+                <summary class="details__summary">
+                    <span>Size</span>
+                </summary>
+                <form class="details__content">
+                    <div class="checkbox-wrapper">
+                        <input type="checkbox" name="gender" value="man">
+                        <span>Man</span>
+                    </div>
+
+                    <div class="checkbox-wrapper">
+                        <input type="checkbox" name="gender" value="woman">
+                        <span>Woman</span>
+                    </div>
+                </form>
+            </details>
+            <details class="details__item">
+                <summary class="details__summary">
+                    <span>Gender</span>
+                </summary>
+                <form class="details__content">
+                    <div class="checkbox-wrapper">
+                        <input type="checkbox" name="gender" value="man">
+                        <span>Man</span>
+                    </div>
+
+                    <div class="checkbox-wrapper">
+                        <input type="checkbox" name="gender" value="woman">
+                        <span>Woman</span>
+                    </div>
                 </form>
             </details>
 
         </div>
         <div class="collection__content">
-            <h2 class="collection__title">{Category's Name}</h2>
+            <h2 class="collection__title">Soccer</h2>
             <ul class="collection__list-products">
-                <li class="product">
-                    <div class="product__image"><img src="/asset/TestingImages/testing-product.png" alt="Product Image">
-                    </div>
-                    <div class="product__info">
-                        <div class="product__title">
-                            <h3>Shin Guard</h3>
-                        </div>
-                        <div class="product__price">
-                            <p>23.33$</p>
-                        </div>
-                    </div>
-                </li>
-
                 <li class="product">
                     <div class="product__image"><img src="/asset/TestingImages/testing-product.png" alt="Product Image">
                     </div>
