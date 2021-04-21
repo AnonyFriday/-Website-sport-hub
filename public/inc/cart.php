5a4e72ac3c63b5f9b24c9d3c7b1c35afafa37249
Vu Kim Duy <duyvukim752000@gmail.com>
2021-04-21 11:46:58 +1000

    [MODIFED] configure some import at cart.php file

--- public/inc/cart.php
+++ public/inc/cart.php
@@ -1,10 +1,13 @@
 <!-- HEADER -->
 
-<?php include_once(SHARED_PATH . "/component/header.php") ?>
+<?php
+require_once("../../private/initializer.php");
+include_once(SHARED_PATH . "/component/header.php"); ?>
 
 <!-- MAIN -->
 <main>
+
 </main>
 
 <!-- FOOTER -->
-<?php include_once(SHARED_PATH . "/component/footer.php") ?>
+<?php include_once(SHARED_PATH . "/component/footer.php"); ?>
